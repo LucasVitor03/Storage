5bf11e88550b8b56d816f961d4da41b27ce1c56b
VazFabricio <vazfabricioof@gmail.com>
2023-06-24 00:16:25 -0300

    teste commit

---
+++ Migrations/20230624030811_AdicionaTabelaProduto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Storage_Project.Migrations
+{
+    public partial class AdicionaTabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
